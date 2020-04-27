3d457f482e6a248bf6830d5c2940f095a13d1cad
Agostinho Ramos <agostinhopina095@gmail.com>
2020-04-27 04:21:32 +0100

    Fragment for Signin

--- app/src/main/java/pt/ipg/application/testingcovid_19/signinActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/signinActivity.java
@@ -1,14 +1,41 @@
 package pt.ipg.application.testingcovid_19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class signinActivity extends AppCompatActivity {
-
+public class signinActivity  extends Fragment {
+    EditText username, password;
+    TextView forgotpassword;
+    Button signin;
+    CheckBox rememberme;
+    ImageView image;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.signin,container,false);
+        username=view.findViewById(R.id.username);
+        password=view.findViewById(R.id.password);
+        signin=view.findViewById(R.id.signin);
+        rememberme=view.findViewById(R.id.rememberme);
+        forgotpassword=view.findViewById(R.id.forgotpassword);
+        image=view.findViewById(R.id.image);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 }
