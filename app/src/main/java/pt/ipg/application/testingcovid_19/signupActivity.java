4fd13027241f341f6eda0ef8f82d149ffbbd4586
Agostinho Ramos <agostinhopina095@gmail.com>
2020-04-28 04:23:15 +0100

    Fragment for Signup Activity

--- app/src/main/java/pt/ipg/application/testingcovid_19/signupActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/signupActivity.java
@@ -1,14 +1,41 @@
 package pt.ipg.application.testingcovid_19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class signupActivity extends AppCompatActivity {
-
+public class signupActivity extends Fragment {
+    EditText full_name,user_name,email,password,confirm_password;
+    Button sign_up,continue_with_google;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.signup,container,false);
+        full_name = view.findViewById(R.id.full_name);
+        user_name = view.findViewById(R.id.user_name);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        confirm_password = view.findViewById(R.id.confirm_password);
+        sign_up = view.findViewById(R.id.sign_up);
+        continue_with_google = view.findViewById(R.id.continue_with_google);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        continue_with_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 }
