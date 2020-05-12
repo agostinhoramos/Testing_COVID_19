8d79fcd886c9e306b22ef7699c84ebd432c503df
Agostinho Ramos <agostinhopina095@gmail.com>
2020-05-12 02:49:26 +0100

    Create DatabaseOpenHelper.java

---
+++ app/src/main/java/pt/ipg/application/testingcovid_19/DatabaseOpenHelper.java
@@ -0,0 +1,38 @@
+package pt.ipg.application.testingcovid_19;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String NOME_BASE_DADOS = "covid_19.db";
+    private static final int VERSAO_BASE_DADOS = 1;
+    private static final boolean DESENVOLVIMENTO = true;
+
+    public DatabaseOpenHelper(@Nullable Context context) {
+        super(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        DBTableUser userTable = new DBTableUser(db);
+        userTable.create();
+
+        DBTableTest testTable = new DBTableTest(db);
+        testTable.create();
+
+        if (DESENVOLVIMENTO) {
+            seedData(db);
+        }
+    }
+
+    private void seedData(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
