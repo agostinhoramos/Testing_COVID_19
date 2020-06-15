c72846a83196327c1ac35813e691e41ac104b860
Agostinho Ramos <agostinhopina095@gmail.com>
2020-06-15 19:08:21 +0100

    Update SigninActivity.java

--- app/src/main/java/pt/ipg/application/testingcovid_19/SigninActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/SigninActivity.java
@@ -4,28 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.AppComponentFactory;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import pt.ipg.application.testingcovid_19.Others.Validations;
 
 
 public class SigninActivity extends Fragment {
+    public static final String EXTRA_TEXT_SUBMIT_TYPE = "PT.IPG.APPLICATION.TESTINGCOVID_19.EXTRA_TEXT_SUBMIT_TYPE";
     public static final String EXTRA_TEXT_USERNAME = "PT.IPG.APPLICATION.TESTINGCOVID_19.EXTRA_TEXT_USERNAME";
     public static final String EXTRA_TEXT_PASSWORD = "PT.IPG.APPLICATION.TESTINGCOVID_19.EXTRA_TEXT_PASSWORD";
     public static final String EXTRA_TEXT_REMEMBER_ME = "PT.IPG.APPLICATION.TESTINGCOVID_19.EXTRA_TEXT_REMEMBER_ME";
-    public static final String EXTRA_TEXT_FORGOT_PASSWORD = "PT.IPG.APPLICATION.TESTINGCOVID_19.EXTRA_TEXT_FORGOT_PASSWORD";
 
-    private EditText username, password;
-    private CheckBox rememberMe;
-    private TextView forgotPassword;
+    private EditText TextInputUsername, TextInputPassword;
+    private CheckBox RememberMe;
+    private TextView ForgotPassword;
 
     @Nullable
     @Override
@@ -33,10 +32,20 @@ public class SigninActivity extends Fragment {
         Button login;
         View view = inflater.inflate(R.layout.activity_signin, container,false);
 
-        username = (EditText) view.findViewById(R.id.username);
-        password = (EditText) view.findViewById(R.id.password);
-        rememberMe = (CheckBox) view.findViewById(R.id.rememberme);
-        forgotPassword = (TextView) view.findViewById(R.id.forgotpassword);
+        TextInputUsername = (EditText) view.findViewById(R.id.username);
+        TextInputPassword = (EditText) view.findViewById(R.id.password);
+        RememberMe = (CheckBox) view.findViewById(R.id.rememberme);
+        ForgotPassword = (TextView) view.findViewById(R.id.forgotpassword);
+
+        ForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameText = TextInputUsername.getText().toString().trim();
+                Intent intent = new Intent(view.getContext(), ForgotPasswordActivity.class);
+                intent.putExtra(EXTRA_TEXT_USERNAME, usernameText);
+                startActivity(intent);
+            }
+        });
 
         login = view.findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
@@ -48,17 +57,38 @@ public class SigninActivity extends Fragment {
         return view;
     }
 
-    public void loginAuth(View view){
-        String usernameText = username.getText().toString();
-        String passwordText = password.getText().toString();
-        String rememberMeText = rememberMe.getText().toString();
-        String forgotPasswordText = forgotPassword.getText().toString();
+    private void loginAuth(View view){
+        boolean auth = true;
 
-        Intent intent = new Intent(view.getContext(), PatientHomeActivity.class);
-        intent.putExtra(EXTRA_TEXT_USERNAME, usernameText);
-        intent.putExtra(EXTRA_TEXT_PASSWORD, passwordText);
-        intent.putExtra(EXTRA_TEXT_REMEMBER_ME, rememberMeText);
-        intent.putExtra(EXTRA_TEXT_FORGOT_PASSWORD, forgotPasswordText);
-        startActivity(intent);
+        String usernameText = TextInputUsername.getText().toString().trim();
+        String passwordText = TextInputPassword.getText().toString();
+        String rememberMeText = RememberMe.getText().toString();
+
+        // VALIDATION
+        if( usernameText.isEmpty() ){
+            TextInputUsername.setError("Field Username can't be empty!");
+            auth = false;
+        }else if(!Validations.matchUSERNAME().matcher(usernameText).matches()){
+            TextInputUsername.setError("Please enter a valid Username!");
+            auth = false;
+        }
+
+        if( passwordText.isEmpty() ){
+            TextInputPassword.setError("Field Password can't be empty!");
+            auth = false;
+        }else if(!Validations.matchPASSWORD().matcher(passwordText).matches()){
+            TextInputPassword.setError("Please enter a valid password");
+            auth = false;
+        }
+
+        // AUTHENTICATION
+        if( auth ){
+            Intent intent = new Intent(view.getContext(), HomeActivity.class);
+            intent.putExtra(EXTRA_TEXT_SUBMIT_TYPE, "PATIENT-SIGNIN");
+            intent.putExtra(EXTRA_TEXT_USERNAME, usernameText);
+            intent.putExtra(EXTRA_TEXT_PASSWORD, passwordText);
+            intent.putExtra(EXTRA_TEXT_REMEMBER_ME, rememberMeText);
+            startActivity(intent);
+        }
     }
 }
