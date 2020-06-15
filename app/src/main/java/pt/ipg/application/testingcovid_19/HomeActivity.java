9e7e629782af3da8ec6a83213693add7e00564cc
Agostinho Ramos <agostinhopina095@gmail.com>
2020-06-15 19:07:26 +0100

    Create HomeActivity.java

---
+++ app/src/main/java/pt/ipg/application/testingcovid_19/HomeActivity.java
@@ -0,0 +1,24 @@
+package pt.ipg.application.testingcovid_19;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_panel);
+
+        Intent intent = getIntent();
+        String usernameText = intent.getStringExtra(SigninActivity.EXTRA_TEXT_USERNAME);
+        String passwordText = intent.getStringExtra(SigninActivity.EXTRA_TEXT_PASSWORD);
+        String rememberMeText = intent.getStringExtra(SigninActivity.EXTRA_TEXT_REMEMBER_ME);
+
+        TextView textView1 = (TextView) findViewById(R.id.myTextView);
+        textView1.setText(usernameText);
+        // TODO
+    }
+}
