2f2f6ed579ac329c7866250c3a44a9fdc189919c
Agostinho Ramos <agostinhopina095@gmail.com>
2020-06-16 05:35:16 +0100

    RecyclerView.Adapter not solved yet

---
+++ app/src/main/java/pt/ipg/application/testingcovid_19/DB/userAdapter.java
@@ -0,0 +1,41 @@
+package pt.ipg.application.testingcovid_19.DB;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/*
+public class userAdapter extends RecyclerView.Adapter<userAdapter.ViewHolderUser> {
+    private final Context context;
+    private Cursor cursor = null;
+
+    public void setCursor(Cursor cursor){
+        if( cursor != this.cursor ){
+            this.cursor = cursor;
+            notifyDataSetChanged();
+        }
+    }
+
+    public userAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderUser onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderUser viewHolderUser, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+*/
