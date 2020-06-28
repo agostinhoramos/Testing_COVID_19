00cac0454b1927467193bdf58338db36c2af776c
Agostinho Ramos <agostinhopina095@gmail.com>
2020-06-28 22:05:18 +0100

    Update DBTableQuestionChoices.java

--- app/src/main/java/pt/ipg/application/testingcovid_19/database/table/DBTableQuestionChoices.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/database/table/DBTableQuestionChoices.java
@@ -4,6 +4,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class DBTableQuestionChoices {
     private SQLiteDatabase db;
+
     public DBTableQuestionChoices(SQLiteDatabase db) {
         this.db = db;
     }
