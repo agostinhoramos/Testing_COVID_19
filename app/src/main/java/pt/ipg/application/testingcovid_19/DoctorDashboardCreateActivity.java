a5bd17f65bbcb69d73902cbc9984e2a58e708c6b
Agostinho Ramos <agostinhopina095@gmail.com>
2020-07-07 02:51:11 +0100

    Update DoctorDashboardCreateActivity.java
    Can send question.
    Validation done for ToggleButton.

--- app/src/main/java/pt/ipg/application/testingcovid_19/DoctorDashboardCreateActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/DoctorDashboardCreateActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,12 +30,14 @@ import pt.ipg.application.testingcovid_19.object.Choice;
 import pt.ipg.application.testingcovid_19.object.Question;
 
 
-public class DoctorDashboardCreateActivity extends Fragment {
+public class DoctorDashboardCreateActivity extends Fragment implements
+        AdapterView.OnItemSelectedListener{
 
     private final static int MIN_WEIGHT = 0;
     private final static int MAX_WEIGHT = 5;
 
-    String[] inputType = {"Plain Text", "Input numeric"};
+    String[] optionType = {"ToggleButton", "CheckBox", "RadioButton", "TextView", "Number"};
+
     private Button btn_add_option, btn_save;
 
     private EditText editTextQuestion;
@@ -45,12 +48,19 @@ public class DoctorDashboardCreateActivity extends Fragment {
     ArrayList<TextView> textViews = new ArrayList<>();
     ArrayList<LinearLayout> ListOptionGroupLayout = new ArrayList<>();
 
-    private TextView textView;
-    private int num_option_plainText = 0;
+    private int num_option = 0;
 
     private LinearLayout linearLayoutRoot;
     private int position = -1;
     private Context context;
+
+    // it's depend
+    private boolean save_condition = true;
+
+    // local variable
+    private String local_question;
+    private String[] local_option;
+    private Integer[] local_weight;
 
     @Nullable
     @Override
@@ -58,13 +68,9 @@ public class DoctorDashboardCreateActivity extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dashboard_doctor_create, container,false);
 
         context = getContext();
-
         linearLayoutRoot = (LinearLayout) view.findViewById(R.id.layout_optionPlace);
-
         editTextQuestion = (EditText) view.findViewById(R.id.question);
-
         setSpinnerContent(view);
-
         btn_add_option = view.findViewById(R.id.addOption);
         btn_add_option.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -73,61 +79,86 @@ public class DoctorDashboardCreateActivity extends Fragment {
                 addOption(view);
             }
         });
-
         btn_save = view.findViewById(R.id.save);
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // verify if is valid information
+                verifyCondition(position);
 
-                String question = editTextQuestion.getText().toString();
-                int len = editTextOption.size();
+                if(save_condition){
+                    if( position == 0 ){
+                        ToggleButtonToVariable();
+                    }
 
-                String[] option = new String[len];
-                for(int i=0; i<len; i++){
-                    option[i] = editTextOption.get(i).getText().toString();
-                }
-
-                len = textViewWeight.size();
-                Integer[] weight = new Integer[len];
-                for(int i=0; i<len; i++){
-                    weight[i] = Integer.parseInt(textViewWeight.get(i).getText().toString());
-                }
-
-                saveQuestion(question, option, weight);
-
-                // clear all views from layout
-                if( num_option_plainText > 0 ){
-                    editTextQuestion.setText("");
-                    linearLayoutRoot.removeAllViews();
-                    num_option_plainText = 0;
+                    saveQuestion(local_question, local_option, local_weight);
+                    // clear all views from layout
+                    if( num_option > 0 ){
+                        editTextQuestion.setText("");
+                        linearLayoutRoot.removeAllViews();
+                        num_option = 0;
+                    }
+                }else{
+                    Toast.makeText(context, "Error in the fields please check again", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
         return view;
+    }
+
+    private void ToggleButtonToVariable(){
+        local_question = editTextQuestion.getText().toString();
+        int len = editTextOption.size();
+        local_option = new String[len];
+        for(int i=0; i<len; i++){
+            local_option[i] = editTextOption.get(i).getText().toString();
+        }
+        len = textViewWeight.size();
+        local_weight = new Integer[len];
+        for(int i=0; i<len; i++){
+            local_weight[i] = Integer.parseInt(textViewWeight.get(i).getText().toString());
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void addOption(View view){
-        if( true || position == 0 ){ // TODO add others views type...
-            view_plainText(context);
+        if( position == 0 ){
+            make_ToggleButton(context);
         }
     }
 
-    private void setSpinnerContent( View view )
-    {
+    private void verifyCondition(int pos){
+        save_condition = true;
+        String question = editTextQuestion.getText().toString().trim();
+        if(question.isEmpty()|| question.length() < 3){
+            save_condition = false;
+        }
+        if(pos == 0){
+            int len = editTextOption.size();
+            if(len < 2){
+                save_condition = false;
+            }
+            for(int i=0; i<len; i++){
+                String opt = editTextOption.get(i).getText().toString().trim();
+                if(opt.isEmpty()||opt.length()<1){
+                    save_condition = false;
+                }
+            }
+        }
+    }
+
+    private void setSpinnerContent( View view ) {
         Spinner spin = (Spinner) view.findViewById(R.id.spinner_option);
-
-        //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, inputType);
+        spin.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the Options list
+        ArrayAdapter aa = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, optionType);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         //Setting the ArrayAdapter data on the Spinner
         spin.setAdapter(aa);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void view_plainText(Context context){
+    private void make_ToggleButton(Context context){
         // TODO save fragment status..
         // Create a LayoutParams for TextView
         LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams(
@@ -143,8 +174,8 @@ public class DoctorDashboardCreateActivity extends Fragment {
 
         // New textView for weight...
         int pos = textViews.size();
-        textView = new TextView(context);
-        textViews.add(textView);
+        TextView textViewWeight = new TextView(context);
+        textViews.add(textViewWeight);
 
         button.setLayoutParams(new LinearLayout.LayoutParams(
                 80, // Width of TextView
@@ -173,9 +204,9 @@ public class DoctorDashboardCreateActivity extends Fragment {
 
         horizontalGroup.addView(button);
 
-        textView.setText("0");
-        horizontalGroup.addView(textView);
-        textViewWeight.add(textView);
+        textViewWeight.setText("0");
+        horizontalGroup.addView(textViewWeight);
+        this.textViewWeight.add(textViewWeight);
 
         button = new Button(context);
         button.setTag(pos);
@@ -201,7 +232,7 @@ public class DoctorDashboardCreateActivity extends Fragment {
         btnMoreAndLess.add(button);
 
         EditText INPUT = new EditText(context);
-        INPUT.setHint("Option " + (num_option_plainText+1));
+        INPUT.setHint("Option " + (num_option+1));
 
         // Apply the layout parameters to TextView widget
         INPUT.setLayoutParams(ll);
@@ -211,7 +242,7 @@ public class DoctorDashboardCreateActivity extends Fragment {
 
         // ADD View to Layout...
         linearLayoutRoot.addView(horizontalGroup);
-        num_option_plainText++;
+        num_option++;
     }
 
     private void saveQuestion(String question, String[] option, Integer[] weight){
@@ -221,7 +252,7 @@ public class DoctorDashboardCreateActivity extends Fragment {
         obj_question.setQuestion(question);
         obj_question.setFk_doctor(id_doctor);
 
-        //try {
+        try {
             ContentResolver resolver = getActivity().getContentResolver();
             Uri uri = resolver.insert(ContentProvider.QUESTION_ADDRESS, Convert.questionToContentValues(obj_question));
             int id_question = Integer.parseInt(uri.getLastPathSegment());
@@ -232,10 +263,21 @@ public class DoctorDashboardCreateActivity extends Fragment {
                 obj_choice.setFk_question(id_question);
                 resolver.insert(ContentProvider.CHOICES_ADDRESS, Convert.choicesToContentValues(obj_choice));
             }
-           // Toast.makeText(getContext(), "Adicionado com sucesso", Toast.LENGTH_SHORT).show();
-        //} catch (Exception e) {
-          //  System.out.println("Error here..");
-        //}
+           Toast.makeText(getContext(), "Adicionado com sucesso", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) { System.out.println("Something went wrong..."); }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(this.position>-1){
+            Toast.makeText(getContext(), optionType[position] , Toast.LENGTH_LONG).show();
+        }
+        this.position = position;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
