69559483ec517bc7708a5cdd5d79ac35aac55187
Agostinho Ramos <agostinhopina095@gmail.com>
2020-07-11 00:29:59 +0100

    Implementation of Terms and conditions

--- app/src/main/java/pt/ipg/application/testingcovid_19/ApplicationStartActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/ApplicationStartActivity.java
@@ -2,6 +2,7 @@ package pt.ipg.application.testingcovid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ApplicationStartActivity extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class ApplicationStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_start);
+        chooseActivityOnStart();
+    }
+
+    private void chooseActivityOnStart(){
+        Intent intent = null;
+        int page_id = -1;
+
+        switch(page_id) {
+            case 0:
+                intent = new Intent(getApplicationContext(), DoctorDashboardActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(getApplicationContext(), UserDashboardActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                intent = new Intent(getApplicationContext(), TermsConditionsActivity.class);
+                startActivity(intent);
+        }
     }
 }
--- app/src/main/java/pt/ipg/application/testingcovid_19/TermsConditionsActivity.java
+++ app/src/main/java/pt/ipg/application/testingcovid_19/TermsConditionsActivity.java
@@ -2,13 +2,46 @@ package pt.ipg.application.testingcovid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TermsConditionsActivity extends AppCompatActivity {
+
+    private Button btn_agree;
+    private Button btn_exit;
+
+    private Intent intent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_conditions);
+
+        btn_agree = findViewById(R.id.btn_agree);
+        btn_exit = findViewById(R.id.btn_exit);
+
+        btn_agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                terms_accepted();
+                intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
+    }
+
+    public void terms_accepted(){
+        //TODO remember user decision..
     }
 }
